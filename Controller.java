e611d116629bce68a996b6830025a9ba7a519097
CitronZoegas <81166713+CitronZoegas@users.noreply.github.com>
2022-02-16 23:01:11 +0100

    UI and FUnctionality

--- Controller.java
+++ Controller.java
@@ -1,20 +1,24 @@
 package controller;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.net.URL;
 import java.util.Objects;
-import java.util.ResourceBundle;
 
-public class Controller implements Initializable {
+public class Controller{
 
     @FXML
     private Stage stage;
@@ -22,66 +26,111 @@ public class Controller implements Initializable {
     private Scene scene;
     @FXML
     private Parent root;
+    @FXML
+    private AnchorPane mainPane;
+    @FXML
+    private Text quoteOfTheDay;
 
     public void addWebsiteScene(ActionEvent event) {
+
         try {
-            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("addWebsite.fxml")));
+
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/FXML/addWebsite.fxml")));
             stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
             scene = new Scene(root);
+            scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/CSS/stylesheets.css")).toExternalForm());
             stage.setScene(scene);
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
     public void removeWebsiteScene(ActionEvent event) {
         try {
-            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("removeWebsite.fxml")));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/removeWebsite.fxml"));
+            Parent root = loader.load();
             stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
             scene = new Scene(root);
+            scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/CSS/stylesheets.css")).toExternalForm());
             stage.setScene(scene);
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
     public void mainPageScene(ActionEvent event) {
         try {
-            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("mainPage.fxml")));
+
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/FXML/mainPage.fxml")));
             stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
             scene = new Scene(root);
+            scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/CSS/stylesheets.css")).toExternalForm());
             stage.setScene(scene);
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
     public void findFileScene(ActionEvent event) {
         try {
-            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("findFileScene.fxml")));
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/FXML/findFile.fxml")));
             stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
             scene = new Scene(root);
+            scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/CSS/stylesheets.css")).toExternalForm());
             stage.setScene(scene);
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
     public void settingsPageScene(ActionEvent event) {
         try {
-
-            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("settingsPage.fxml")));
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/FXML/settingsPage.fxml")));
             stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
             scene = new Scene(root);
+            scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/CSS/stylesheets.css")).toExternalForm());
             stage.setScene(scene);
             stage.show();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+    public void fillMainPageWithWebsites() {
+        String str = null;
+        try{
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+            Document doc = Jsoup.connect("https://www.azquotes.com/").userAgent("Mozilla/17.0").get();
+            Elements element = doc.select("div.content-slide content-slide_top");
+
+            int i = 0;
+            for(Element quotesList:element){
+                System.out.println(i + Objects.requireNonNull(quotesList.getElementsByTag("a").first()).text());
+                str = i + (Objects.requireNonNull(quotesList.getElementsByTag("a").first()).text());
+                i++;
+                quoteOfTheDay.setText(str);
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
+
+    public void exitApplication() {
+        Platform.exit();
+        System.exit(0);
+    }
+    public void setMinimized(ActionEvent event) {
+        System.out.println("lol");
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        stage.setIconified(true);
+    }
+
+
+
 }
--- Main.java
+++ Main.java
@@ -1,35 +1,41 @@
 package controller;
 
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
-        Platform.setImplicitExit(false);
+
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("../FXML/mainPage.fxml"));
+        //Controller controller = fxmlLoader.getController();
         Parent root = fxmlLoader.load();
-        primaryStage.getIcons().add(new Image("icons/AppIcon.png"));
+        updateQuotePanel();
         Scene scene = new Scene(root, 613, 702);
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/CSS/stylesheets.css")).toExternalForm());
+
+        primaryStage.getIcons().add(new Image("icons/AppIcon.png"));
+        primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.setTitle("WWWControllerV2");
         primaryStage.setResizable(false);
-        primaryStage.setOnCloseRequest(e->{
-            Platform.exit();
-            System.exit(0);
-        });
-
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
     public static void main(String[] args) {
         launch();
+    }
+    public void updateQuotePanel() {
+        Controller controller = new Controller();
+        controller.fillMainPageWithWebsites();
+
     }
 }
